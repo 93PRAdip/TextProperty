12a1eb21ced6535be8e0960e7b2bc6bc60e9fd71
Pradip <pradip@pradips-mac.local>
2015-10-17 12:36:06 +0530

    first Commit

--- TextProperty/ViewController.swift
+++ TextProperty/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func setColour(sender: AnyObject) {
+        Label.textColor=UIColor.redColor()
+    }
+  
+    @IBAction func setFontSize(sender: AnyObject) {
+        Label.font=UIFont(name: "Vendra", size: 25)
+    }
 
+    @IBAction func setShadow(sender: AnyObject) {
+        Label.layer.shadowColor = UIColor.blackColor().CGColor
+        Label.layer.shadowOffset = CGSizeMake(2, 2)
+        Label.layer.shadowRadius = 2
+        Label.layer.shadowOpacity = 0.25
+        
+    }
+    @IBAction func  alignLeft(sender: AnyObject){
+        Label.textAlignment = NSTextAlignment.Left
+    }
+    @IBAction func  alignRight(sender:AnyObject){
+        Label.textAlignment = NSTextAlignment.Right
+    }
+    @IBAction func alignCenter(sender:AnyObject){
+        Label.textAlignment = NSTextAlignment.Center
+    }
 }
 
